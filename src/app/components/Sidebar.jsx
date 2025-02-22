fd1446d545bf1e77def86a5504cd7f646fc680ea
opeyemi <opeyemiodunayo2018@gmail.com>
2025-02-22 03:32:39 +0100

    initial commit

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div className="text-white">Sidebar</div>;
+  return <div className="text-white">Hope drive</div>;
 };
 
 export default Sidebar;
